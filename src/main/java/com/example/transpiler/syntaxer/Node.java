02b6ded9bef1e0493eb6ec0419f550993723b070
Maxim Kozhinov <maxim.kozhinov@waveaccess.global>
2022-04-12 21:56:24 +0300

    Structure for syntaxer added

---
+++ src/main/java/com/example/transpiler/syntaxer/Node.java
@@ -0,0 +1,34 @@
+package com.example.transpiler.syntaxer;
+
+import java.util.ArrayList;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Node {
+    private FormalGrammar type;
+    private String value;
+    private List<Node> childNodes;
+
+    public Node(FormalGrammar formalGrammar) {
+        childNodes = new ArrayList<>();
+        type = formalGrammar;
+    }
+
+    public Node(FormalGrammar formalGrammar, String value) {
+        childNodes = new ArrayList<>();
+        type = formalGrammar;
+        this.value = value;
+    }
+
+    public Node addChild(Node childNode) {
+        childNodes.add(childNode);
+        return childNode;
+    }
+
+    public void deleteLastChild() {
+        childNodes.remove(childNodes.size() - 1);
+    }
+}
---
+++ src/main/java/com/example/transpiler/syntaxer/Tree.java
@@ -0,0 +1,20 @@
+package com.example.transpiler.syntaxer;
+
+import lombok.Data;
+
+@Data
+public class Tree {
+    private final Node root;
+
+    public Tree() {
+        root = new Node(FormalGrammar.PROGRAM);
+    }
+
+    public Node addNode(FormalGrammar formalGrammar, Node parentNode) {
+        return parentNode.addChild(new Node(formalGrammar));
+    }
+
+    public void addNode(FormalGrammar formalGrammar, String value, Node parentNode) {
+        parentNode.addChild(new Node(formalGrammar, value));
+    }
+}
